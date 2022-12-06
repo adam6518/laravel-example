7bb38aeb6dd94149c05d2f3aad8c82737dd369d7
adam6518 <getamped75@gmail.com>
2022-12-07 01:22:59 +0700

    tp1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <title>Kursus Stir Mobil Jakarta</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div id="logo" class="logo">
+        <h1>Let's Drive With Us !</h1>
+    </div>
+    <div id="logo">
+        <img src="img/logo.png"/>
+    </div>
+    <div id="logo">
+        <h1>Pilihan Jadwal Kursus</h1>
+        <table class="table table-dark table-striped container">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Tanggal</th>
+      <th scope="col">Jam Latihan</th>
+      <th scope="col">Durasi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>01 - 15 Desember 2022</td>
+      <td>09.00 - 11.00 WIB</td>
+      <td>2 Jam</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>16 - 31 Desember 2022</td>
+      <td>12.00 - 15.00 WIB</td>
+      <td>3 Jam</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>01 - 31 Desember 2022</td>
+      <td>13.00-14.00 WIB</td>
+      <td>1 Jam`</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    <div id="logo">
+        <h1>Pilihan Paket Kursus</h1>
+        <table class="table table-success table-striped container">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama Paket</th>
+      <th scope="col">Harga Paket</th>
+      <th scope="col">Waktu Pelatihan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Paket A</td>
+      <td>Rp. 2.500.000</td>
+      <td>3 Jam @sesi</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Paket B</td>
+      <td>Rp. 2.000.000</td>
+      <td>2 Jam @sesi</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Paket C</td>
+      <td>Rp. 1.500.000</td>
+      <td>1 Jam @sesi</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    <div id="logo">
+        <h1>Trainer Kami</h1>
+        <div class="card container" style="width: 18rem;">
+            <img id="img-trainer" src="/img/trainer.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Meet Our Trainer !</h5>
+                <p class="card-text">Hello, I'm Mr. Anton. Let's learn with me !</p>
+                <a href="#" class="btn btn-primary">Choose Him</a>
+    </div>
+</div>
+    </div>
+    <div id="logo">
+        <h1>Jenis Kendaraan</h1>
+            <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Matic Transmission</h5>
+                <img id="img-transmission" src="img/matic.jpg"/>
+                <a href="#" class="btn btn-primary">Choose</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Manual Transmission</h5>
+                <img id="img-transmission" src="img/manual.jpg"/>
+                <a href="#" class="btn btn-primary">Choose</a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div id="logo">
+        <h1>Contact Us</h1>
+        <h4>Phone    : 021-555-9990</h4>
+        <h4>Whatsapp : 08123456789</h4>
+        <h4>Location : <br>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6664270097594!2d106.82496411476893!3d-6.175392395529166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5d2e764b12d%3A0x3d2ad6e1e0e9bcc8!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1670349905483!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </h4>
+    </div>
+</body>
+</html>
